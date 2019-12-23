d0adb8b728cccb178fdc73073ce46b5754a6bc25
Magno Sescon <magnosescon@gmail.com>
2019-12-23 16:49:34 -0300

    apenas atualizando os testes

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -22,6 +22,18 @@ export class CalculadoraService {
 
   constructor() { }
 
+  /**
+   * Método que calcula uma operação matemática dado
+   * dois números.
+   * Suporta as operações soma, subtração, divisão e multiplicação.
+   * 
+   * @param num1 number
+   * @param num2 number
+   * @param operacao string Operação a ser executada
+   * @return number Resultado da Operação
+   */
+
+
   calcular(num1: number, num2: number, operacao:string): number {
   
     let resultado: number; //armazena o resultado da operação
